fc721ed835ecb51ead2601095e4eebb6ce90265f
Someone <gorgonnash@yahoo.com>
2016-01-10 22:30:58 -0800

    SignalR setup

---
+++ AngularDotNet/SignalRChat/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace SignalRChat
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/signalr").Include(
+                        "~/Scripts/jquery.signalR-2.2.0.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));            
+        }
+    }
+}
---
+++ AngularDotNet/SignalRChat/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// Sample use case from client:
+        /// $.connection.chat.client.hello = function () { console.log('hello from client!'); };
+        /// $.connection.hub.start();
+        /// $.connection.chat.server.hello();
+        /// </summary>
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+        /// <summary>
+        /// Sample use case from client:
+        /// $.connection.chat.client.speak = function (msg) { console.log(msg); };
+        /// $.connection.hub.start();
+        /// $.connection.chat.server.saySomething('I am on client!');
+        /// </summary>
+        public void SaySomething(string message)
+        {
+            Clients.All.speak(message + " - from the server.");
+        }
+    }
+}
