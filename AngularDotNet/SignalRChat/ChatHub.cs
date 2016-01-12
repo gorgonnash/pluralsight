163fe35eaee9337b050211814b5bd9af9da2ce8b
Someone <gorgonnash@yahoo.com>
2016-01-12 00:13:33 -0800

    Hub Groups & Context

--- AngularDotNet/SignalRChat/ChatHub.cs
+++ AngularDotNet/SignalRChat/ChatHub.cs
@@ -6,6 +6,22 @@ namespace SignalRChat
     [HubName("chat")]
     public class ChatHub : Hub
     {
+        public void SendMessage(SendData data)
+        {
+            var msg = string.Format("[{0}]: {1}", data.Name, data.Message);
+            Clients.Group(data.RoomName).newMessage(msg);
+        }
+
+        public void JoinRoom(string roomName) 
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public void LeaveRoom(string roomName)
+        {
+            Groups.Remove(Context.ConnectionId, roomName);
+        }
+
         /// <summary>
         /// Sample use case from client:
         /// $.connection.chat.client.hello = function () { console.log('hello from client!'); };
@@ -17,7 +33,6 @@ namespace SignalRChat
             Clients.All.hello();
         }
 
-
         /// <summary>
         /// Sample use case from client:
         /// $.connection.chat.client.speak = function (msg) { console.log(msg); };
@@ -28,10 +43,14 @@ namespace SignalRChat
         {
             Clients.All.speak(message + " - from the server.");
         }
+    }
 
-        public void SendMessage(string message)
-        {
-            Clients.All.newMessage(message);
-        }
+    public class SendData
+    {
+        public string Message { get; set; }
+
+        public string RoomName { get; set; }
+
+        public string Name { get; set; }
     }
 }
--- AngularDotNet/SignalRChat/app/mainCtrl.js
+++ AngularDotNet/SignalRChat/app/mainCtrl.js
@@ -1,8 +1,31 @@
 ﻿angular.module('app').controller('mainCtrl', function ($scope, chat) {
     $scope.messages = [];
 
+    $scope.inRoom = false;
+    $scope.nameSet = false;
+
+    $scope.setName = function () {
+        $scope.nameSet = true;
+    };
+
+    $scope.joinRoom = function () {
+        $scope.inRoom = true;
+        chat.server.joinRoom($scope.roomName);
+    };
+
+    $scope.leaveRoom = function () {
+        $scope.inRoom = false;
+        chat.server.leaveRoom($scope.roomName);
+    };
+
     $scope.sendMessage = function () {
-        chat.server.sendMessage('hi!');
+        chat.server.sendMessage({ message: $scope.newMessage, roomName: $scope.roomName, name: $scope.name });
         $scope.newMessage = "";
     }
+
+    chat.client.newMessage = function onNewMessage(message) {
+        $scope.messages.push({ message: message });
+        $scope.$apply();
+        console.log(message);
+    };
 });
