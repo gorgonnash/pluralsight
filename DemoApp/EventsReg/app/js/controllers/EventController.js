89919ce88774035320792029b5fc5b0dfc3de67c
Someone <gorgonnash@yahoo.com>
2016-01-19 00:03:53 -0800

    Event handling

--- DemoApp/EventsReg/app/js/controllers/EventController.js
+++ DemoApp/EventsReg/app/js/controllers/EventController.js
@@ -11,6 +11,19 @@ eventsApp.controller('EventController', function EventController($scope) {
             city: 'Redmond',
             state: 'WA'
         },
-        imageUrl: '/img/angularjs-logo.png'
+        imageUrl: '/img/angularjs-logo.png',
+        sessions: [
+            { name: 'Directives 101', creatorName: 'Bob', duration: '1 hr', level: 'Advanced', abstract: 'As you wish.', upVoteCount: 0 },
+            { name: 'Scopes everything', creatorName: 'Joe', duration: '2 hrs', level: 'Introductory', abstract: 'Yesss!', upVoteCount: 0 },
+            { name: 'Controllers mastery', creatorName: 'Mary', duration: '3 hrs', level: 'Intermediate', abstract: 'Pwned!', upVoteCount: 0 }
+        ]
+    };
+
+    $scope.upVoteSession = function (session) {
+        session.upVoteCount++;
+    };
+
+    $scope.downVoteSession = function (session) {
+        session.upVoteCount--;
     };
 });
